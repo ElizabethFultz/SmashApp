f83df8923a18ef9609ef4043f53e9330c1d09565
Aaron Bone <aaronbn10@gmail.com>
2021-11-30 11:43:34 -0500

    Add files via upload
    11/29/21 - Aaron - Made no progress! - 6 hours

---
+++ Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Player
+{
+    String username;
+    String charac;
+    String state;
+	public Player(string userN, string ch, string sta)
+    {
+        username = userN;
+        charac = ch;
+        state = sta;
+    } //end player const
+
+
+}//end class player
+
