3be16829411a15fd15283ed6c39afc8e7ce94f00
Jaylon Kiper <46461179+jlkiper@users.noreply.github.com>
2021-12-01 13:14:44 -0500

    Add files via upload
    12/1/21 - Aaron/Jaylon - Worked on button functionality - 3 hours

---
+++ SmashApp-master/SmashApp-master/SmashApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmashApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MatchReporting_Click(object sender, RoutedEventArgs e)
+        {
+            MatchReporting1_UserValidation userValidation = new MatchReporting1_UserValidation();
+            this.Visibility = Visibility.Hidden;
+            userValidation.Show();
+       
+        }
+
+        private void User_Profile_Click(object sender, RoutedEventArgs e)
+        {
+            UserProfile profile = new UserProfile();
+            this.Visibility = Visibility.Hidden;
+            profile.Show();
+        }
+    }
+}
