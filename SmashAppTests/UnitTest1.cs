d3e18f13b401f9300f8d666df4482a6e74ea353a
Aaron Bone <aaronbn10@gmail.com>
2021-12-01 20:07:31 -0500

    12/1/21 - Jaylon - Added reworked tests - 8:07PM

--- SmashAppTests/UnitTest1.cs
+++ SmashAppTests/UnitTest1.cs
@@ -14,10 +14,36 @@ namespace SmashAppTests
 
         }
 
-        [Test]
+       [Test]
         public void Test1()
         {
-            Assert.Pass();
+            string settest = "";
+
+            Set set1 = new();
+            set1.p1 = "Aaron";
+            set1.p2 = "Jaylon";
+            set1.p1char = "Mario";
+            set1.p2char = "Cloud";
+            set1.stage = "Battlefield";
+            set1.gameTotal--;
+            set1.p1Win = 1;
+            set1.p2Win = 0;
+
+            Assert.Pass(settest, set1);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string playertest = "";
+
+            Player play = new();
+            play.userName = "Jaylon";
+            play.favCharacter = "Sephiroth";
+            play.state = "Kentucky";
+            play.city = "Louisville";
+
+            Assert.Pass(playertest, play);
         }
     }
 }
